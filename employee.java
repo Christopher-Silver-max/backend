c7a61f30c5374bf70638fc466817f93cdffcfa57
Christopher-Silver-max <74786755+Christopher-Silver-max@users.noreply.github.com>
2020-11-20 14:59:33 -0500

    Add files via upload

--- employee.java
+++ employee.java
@@ -2,16 +2,16 @@ public class Employee{
 	private string name;
 	private string id;
 	private Manager manager;
-	Employee (string name, string id, Manager manager){
+	Employee (string name, string id, Manager manager){ // changing file 
 			name = name;
 			id = id;
 			manager = aCompany;
 		}	
 	public String toString ()
 	{
-	return String.format(“%s, %s, %s, %s, %s, %s”,
-	“Employee name”, name,
-	“Employee Id”, id,
-	“Manager name”, manager.getName());
+	return String.format(ï¿½%s, %s, %s, %s, %s, %sï¿½,
+	ï¿½Employee nameï¿½, name,
+	ï¿½Employee Idï¿½, id,
+	ï¿½Manager nameï¿½, manager.getName());
 	}
 }
